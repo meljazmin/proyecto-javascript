ed23d50ff46cd999127a49ab70863c03651f65fa
Melanie <meeelu.monti17@hotmail.com>
2021-01-21 15:53:29 -0300

    Cambiando objetos y diseño

--- js/main.js
+++ js/main.js
@@ -22,18 +22,18 @@ function aplicarDescuento() {
 function cargarProductos() {
 
     let productosObjeto = [];
-    productosObjeto.push(new Producto("Moño", "Rojo", 150, 100, 'imagenes/moño.jpg'));
-    productosObjeto.push(new Producto("Moño", "Rosa", 150, 100, 'imagenes/moño2.jpg'));
-    productosObjeto.push(new Producto("Moño", "Dorado", 250, 80, 'imagenes/snake-necklace.jpg'));
-    productosObjeto.push(new Producto("Collar", "Moon", 450, 80, 'imagenes/snake-necklace.jpg'));
-    productosObjeto.push(new Producto("Collar", "Místico", 500, 70, 'imagenes/snake-necklace.jpg'));
-    productosObjeto.push(new Producto("Collar", "Dije de víbora", 700, 60, 'imagenes/snake-necklace.jpg'));
-    productosObjeto.push(new Producto("Mochila", "Rosa", 2300, 50, 'imagenes/snake-necklace.jpg'));
-    productosObjeto.push(new Producto("Moño", "Conejito", 2400, 40, 'imagenes/snake-necklace.jpg'));
-    productosObjeto.push(new Producto("Anillo", "Metal", 1000, 90, 'imagenes/snake-necklace.jpg'));
-    productosObjeto.push(new Producto("Anillo", "Oro", 6000, 40, 'imagenes/snake-necklace.jpg'));
-    productosObjeto.push(new Producto("Anillo", "Plata", 3000, 30, 'imagenes/snake-necklace.jpg'));
-    productosObjeto.push(new Producto("Anillo", "Amatista", 3000, 30, 'imagenes/snake-necklace.jpg'));
+    productosObjeto.push(new Producto("Moño", "Rojo", 150, 100, 'imagenes/moño-rojo.jpg'));
+    productosObjeto.push(new Producto("Moño", "Rosa", 150, 100, 'imagenes/moño-rosa.jpg'));
+    productosObjeto.push(new Producto("Moño", "Dorado", 250, 80, 'imagenes/moño-dorado.jpg'));
+    productosObjeto.push(new Producto("Collar", "Moon", 450, 80, 'imagenes/collar-moon.webp'));
+    productosObjeto.push(new Producto("Collar", "Místico", 500, 70, 'imagenes/collar-mistico.jpg'));
+    productosObjeto.push(new Producto("Collar", "Víbora", 700, 60, 'imagenes/snake-necklace.jpg'));
+    productosObjeto.push(new Producto("Mochila", "Rosa", 2300, 50, 'imagenes/mochila-rosa.jpg'));
+    productosObjeto.push(new Producto("Mochila", "Conejito", 2400, 40, 'imagenes/mochila-conejito.jpg'));
+    productosObjeto.push(new Producto("Anillo", "Diamantes", 1000, 90, 'imagenes/anillo-diamante.jpg'));
+    productosObjeto.push(new Producto("Anillo", "Oro", 6000, 40, 'imagenes/anillo-oro.jpg'));
+    productosObjeto.push(new Producto("Anillo", "Plata", 3000, 30, 'imagenes/anillo-plata.jpg'));
+    productosObjeto.push(new Producto("Anillo", "Amatista", 3000, 30, 'imagenes/anillo-amatista.jpg'));
 
     //Recorriendo lista de productos
     const carritoDiv = document.getElementById("carrito");
@@ -41,10 +41,10 @@ function cargarProductos() {
     for (let i = 0; i < productosObjeto.length; i++) {
         let producto = productosObjeto[i];
         carritoDiv.innerHTML += `
-        <div class="col-md-3 text-center">
-            <img src="${producto.imagen}" alt="${producto.tipoDeProducto} ${producto.nombre}" title="${producto.nombre}" width="50%" height="50%" />
+        <div class="col-md-3 text-center" style="margin-top: 20px">
+            <img src="${producto.imagen}" alt="${producto.tipoDeProducto} ${producto.nombre}" title="${producto.nombre}" width="60%" height="60%" />
             <h2>${producto.tipoDeProducto} ${producto.nombre}</h2>
-            <p>Precio: $${producto.precio}</p>
+            <p>ARS$${producto.precio}</p>
             <p>Disponible: ${producto.stock}</p>
         </div>
         `;
@@ -77,8 +77,7 @@ function cargarPantallaContactos() {
 // new WOW().init();
 window.onload = function (event) {
     // event.preventDefault();
-
-    alert("¡Suscribite a nuestro newsletter!");
+    bootbox.alert('¡Suscribite a nuestro newsletter!');
 
     cargarProductos();
     cargarAnimacionOferta();
