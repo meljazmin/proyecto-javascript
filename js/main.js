c6cedc7c5db7e2036862f423d73b64279bb3ac83
Melanie <meeelu.monti17@hotmail.com>
2021-01-21 01:14:58 -0300

    Agregando objetos

--- js/main.js
+++ js/main.js
@@ -20,58 +20,35 @@ function aplicarDescuento() {
 }
 
 function cargarProductos() {
-    let productos = ["Moño", "Collares", "Mochilas", "Anillos"];
-    let monios = ["Rojo", "Rosa", "Dorado"];
-    let collares = ["Moon", "Místico", "Dije con víbora"];
-    let mochilas = ["Rosa", "Conejito"];
-    let anillos = ["Metal", "Oro", "Plata"];
 
-    function mostrarListaProducto(event) {
-        event.preventDefault();
-        let listadoDeProductos = document.getElementById('listaProducto');
-        listadoDeProductos.innerHTML = "";
-        let listaProducto;
-        switch (event.target.innerText) {
-            case "Moño":
-                listaProducto = monios;
-                break;
-            case "Collares":
-                listaProducto = collares;
-                break;
-            case "Mochilas":
-                listaProducto = mochilas;
-                break;
-            case "Anillos":
-                listaProducto = anillos;
-                break;
-        }
-        let listaProductoElem = document.createElement('ul');
-        for (let j = 0; j < listaProducto.length; j++) {
-            let liElement = document.createElement('li');
-            liElement.innerText = listaProducto[j];
-            listaProductoElem.appendChild(liElement);
-        }
-
-        listadoDeProductos.appendChild(listaProductoElem);
-
-
-    }
+    let productosObjeto = [];
+    productosObjeto.push(new Producto("Moño", "Rojo", 150, 100, 'imagenes/moño.jpg'));
+    productosObjeto.push(new Producto("Moño", "Rosa", 150, 100, 'imagenes/moño2.jpg'));
+    productosObjeto.push(new Producto("Moño", "Dorado", 250, 80, 'imagenes/snake-necklace.jpg'));
+    productosObjeto.push(new Producto("Collar", "Moon", 450, 80, 'imagenes/snake-necklace.jpg'));
+    productosObjeto.push(new Producto("Collar", "Místico", 500, 70, 'imagenes/snake-necklace.jpg'));
+    productosObjeto.push(new Producto("Collar", "Dije de víbora", 700, 60, 'imagenes/snake-necklace.jpg'));
+    productosObjeto.push(new Producto("Mochila", "Rosa", 2300, 50, 'imagenes/snake-necklace.jpg'));
+    productosObjeto.push(new Producto("Moño", "Conejito", 2400, 40, 'imagenes/snake-necklace.jpg'));
+    productosObjeto.push(new Producto("Anillo", "Metal", 1000, 90, 'imagenes/snake-necklace.jpg'));
+    productosObjeto.push(new Producto("Anillo", "Oro", 6000, 40, 'imagenes/snake-necklace.jpg'));
+    productosObjeto.push(new Producto("Anillo", "Plata", 3000, 30, 'imagenes/snake-necklace.jpg'));
+    productosObjeto.push(new Producto("Anillo", "Amatista", 3000, 30, 'imagenes/snake-necklace.jpg'));
 
     //Recorriendo lista de productos
-    let listaTipoProductoElem = document.createElement('ul');
+    const carritoDiv = document.getElementById("carrito");
 
-    for (let i = 0; i < productos.length; i++) {
-        let productoLi = document.createElement('li');
-        // productoLi.innerText = productos[i];
-        let link = document.createElement('a');
-        link.innerText = productos[i];
-        link.href = "";
-        link.onclick = mostrarListaProducto;
-        productoLi.appendChild(link);
-        listaTipoProductoElem.appendChild(productoLi);
+    for (let i = 0; i < productosObjeto.length; i++) {
+        let producto = productosObjeto[i];
+        carritoDiv.innerHTML += `
+        <div class="col-md-3 text-center">
+            <img src="${producto.imagen}" alt="${producto.tipoDeProducto} ${producto.nombre}" title="${producto.nombre}" width="50%" height="50%" />
+            <h2>${producto.tipoDeProducto} ${producto.nombre}</h2>
+            <p>Precio: $${producto.precio}</p>
+            <p>Disponible: ${producto.stock}</p>
+        </div>
+        `;
     }
-
-    document.getElementById("listado").appendChild(listaTipoProductoElem);
 }
 
 function cargarAnimacionOferta() {
@@ -107,3 +84,17 @@ window.onload = function (event) {
     cargarAnimacionOferta();
     cargarPantallaContactos();
 }
+
+
+function Producto(tipoDeProducto, nombre, precio, stock, imagen) {
+    this.tipoDeProducto = tipoDeProducto;
+    this.nombre = nombre;
+    this.precio = precio;
+    this.stock = stock;
+    this.imagen = imagen;
+}
+
+
+
+
+
