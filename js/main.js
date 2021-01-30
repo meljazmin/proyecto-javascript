fa7b6792f6426d4a20dda7a8b108e7750a13e6fa
Melanie <meeelu.monti17@hotmail.com>
2021-01-30 20:40:12 -0300

    Agregando funcionalidades

--- js/main.js
+++ js/main.js
@@ -1,8 +1,20 @@
+class Producto {
+    constructor(id, tipoDeProducto, nombre, precio, stock, imagen) {
+        this.id = id;
+        this.tipoDeProducto = tipoDeProducto;
+        this.nombre = nombre;
+        this.precio = precio;
+        this.stock = stock;
+        this.imagen = imagen;
+    }
+}
+
+
 let productosObjeto = [];
 
 function aplicarDescuento() {
     let codDescuento = document.getElementById('descuento').value;
-    let precioProducto = producto.precio;
+    let precioProducto;
     let precioFinal;
     let descuento = 0;
 
@@ -23,18 +35,18 @@ function aplicarDescuento() {
 
 function cargarProductos() {
     productosObjeto = [];
-    productosObjeto.push(new Producto("Moño", "Rojo", 150, 100, 'imagenes/moño-rojo.jpg'));
-    productosObjeto.push(new Producto("Moño", "Rosa", 150, 100, 'imagenes/moño-rosa.jpg'));
-    productosObjeto.push(new Producto("Moño", "Dorado", 250, 80, 'imagenes/moño-dorado.jpg'));
-    productosObjeto.push(new Producto("Collar", "Moon", 450, 80, 'imagenes/collar-moon.webp'));
-    productosObjeto.push(new Producto("Collar", "Místico", 500, 70, 'imagenes/collar-mistico.jpg'));
-    productosObjeto.push(new Producto("Collar", "Víbora", 700, 60, 'imagenes/snake-necklace.jpg'));
-    productosObjeto.push(new Producto("Mochila", "Rosa", 2300, 50, 'imagenes/mochila-rosa.jpg'));
-    productosObjeto.push(new Producto("Mochila", "Conejito", 2400, 40, 'imagenes/mochila-conejito.jpg'));
-    productosObjeto.push(new Producto("Anillo", "Diamantes", 1000, 90, 'imagenes/anillo-diamante.jpg'));
-    productosObjeto.push(new Producto("Anillo", "Oro", 6000, 40, 'imagenes/anillo-oro.jpg'));
-    productosObjeto.push(new Producto("Anillo", "Plata", 3000, 30, 'imagenes/anillo-plata.jpg'));
-    productosObjeto.push(new Producto("Anillo", "Amatista", 3000, 30, 'imagenes/anillo-amatista.jpg'));
+    productosObjeto.push(new Producto(1, "Moño", "Rojo", 150, 100, 'imagenes/moño-rojo.jpg'));
+    productosObjeto.push(new Producto(2, "Moño", "Rosa", 150, 100, 'imagenes/moño-rosa.jpg'));
+    productosObjeto.push(new Producto(3, "Moño", "Dorado", 250, 80, 'imagenes/moño-dorado.jpg'));
+    productosObjeto.push(new Producto(4, "Collar", "Moon", 450, 80, 'imagenes/collar-moon.webp'));
+    productosObjeto.push(new Producto(5, "Collar", "Místico", 500, 70, 'imagenes/collar-mistico.jpg'));
+    productosObjeto.push(new Producto(6, "Collar", "Víbora", 700, 60, 'imagenes/snake-necklace.jpg'));
+    productosObjeto.push(new Producto(7, "Mochila", "Rosa", 2300, 50, 'imagenes/mochila-rosa.jpg'));
+    productosObjeto.push(new Producto(8, "Mochila", "Conejito", 2400, 40, 'imagenes/mochila-conejito.jpg'));
+    productosObjeto.push(new Producto(9, "Anillo", "Diamantes", 1000, 90, 'imagenes/anillo-diamante.jpg'));
+    productosObjeto.push(new Producto(10, "Anillo", "Oro", 6000, 40, 'imagenes/anillo-oro.jpg'));
+    productosObjeto.push(new Producto(11, "Anillo", "Plata", 3000, 30, 'imagenes/anillo-plata.jpg'));
+    productosObjeto.push(new Producto(12, "Anillo", "Amatista", 3000, 30, 'imagenes/anillo-amatista.jpg'));
 }
 
 function mostrarProductos(filtro) {
@@ -55,11 +67,14 @@ function mostrarProductos(filtro) {
     for (let i = 0; i < listaProductos.length; i++) {
         let producto = listaProductos[i];
         carritoDiv.innerHTML += `
-         <div class="col-md-3 text-center" style="margin-top: 20px">
-             <img src="${producto.imagen}" alt="${producto.tipoDeProducto} ${producto.nombre}" title="${producto.nombre}" width="60%" height="60%" />
-             <h2>${producto.tipoDeProducto} ${producto.nombre}</h2>
-             <p>ARS$${producto.precio}</p>
-             <p>Disponible: ${producto.stock}</p>
+         <div data-id="${producto.id}" class="card m-1" style="width: 18rem;">
+             <img class="card-img-top producto-img" src="${producto.imagen}" alt="${producto.tipoDeProducto} ${producto.nombre}" title="${producto.nombre}" />
+             <div class="card-body">
+                <h2 class="card-title">${producto.tipoDeProducto} ${producto.nombre}</h2>
+                <p class="card-text">ARS$${producto.precio}</p>
+                <p class="card-text">Disponible: ${producto.stock}</p>
+                <button class="btn btn-block buttonAgregarCarrito" onClick="agregarAlCarrito(event)"><i class="fas fa-plus"></i></button>
+             </div>
          </div>
          `;
     }
@@ -80,10 +95,10 @@ function cargarPantallaProductos() {
 
 function cargarAnimacionOferta() {
     //Texto que parpadea
-    var color = "#7b38d8";
+    var color = "#bd819c";
     function parpadear() {
         let blink = document.getElementById("blink");
-        color = (color == "#ffffff") ? "#7b38d8" : "#ffffff";
+        color = (color == "#ffffff") ? "#bd819c" : "#ffffff";
         blink.style.color = color;
         blink.style.fontSize = '36px';
     }
@@ -100,33 +115,109 @@ function cargarPantallaContactos() {
     }
 }
 
+function agregarAlCarrito(evt) {
+    const parentElement = evt.target.parentElement.parentElement;
+    const idProducto = $(parentElement).data('id');
+
+    // alert(`Producto ${idProducto}`);
+    const productoAAgregar = productosObjeto.find(producto => producto.id === idProducto);
+    if (productoAAgregar) {
+        let carrito = getCarritoFromStorage();
+        const indexItem = carrito.findIndex(item => productoAAgregar.id === item.id)
+        if (indexItem > -1) {
+            carrito[indexItem].cantidad++;
+        } else {
+            productoAAgregar.cantidad = 1;
+            carrito.push(productoAAgregar);
+        }
+        saveCarritoInStorage(carrito);
+        bootbox.alert(`Se agregó ${productoAAgregar.tipoDeProducto} ${productoAAgregar.nombre} al carrito`);
+    } else {
+        bootbox.alert(`Ha ocurrido un error: No se encontro el producto con id ${idProducto}`);
+    }
+}
+
+function limpiarCarrito() {
+    saveCarritoInStorage();
+}
+
+function getCarritoFromStorage() {
+    let carrito = localStorage.getItem('carrito');
+    if (carrito) {
+        carrito = JSON.parse(carrito);
+    } else {
+        carrito = [];
+    }
+    return carrito;
+}
+
+function saveCarritoInStorage(carrito = []) {
+    window.dispatchEvent(carritoChangedEvent);
+    localStorage.setItem('carrito', JSON.stringify(carrito));
+}
+
+const carritoChangedEvent = new CustomEvent('carritoChanged');
+window.addEventListener('carritoChanged', (evt) => {
+    const carrito = getCarritoFromStorage();
+    document.getElementById('carritoCountSpan').innerText = carrito.reduce((acum, item) =>{
+        acum += item.cantidad;
+        return acum;
+    }, 0);
+});
+
 // Va por afuera del onload porque sino hace que la página se cargue dos veces
 // new WOW().init();
 window.onload = function (event) {
     // event.preventDefault();
     bootbox.alert({
         message: '¡Suscribite a nuestro newsletter!',
-      className: 'rubberBand animated'
+        className: 'rubberBand animated'
     });
+
+    window.dispatchEvent(carritoChangedEvent);
 
     cargarPantallaProductos();
     cargarAnimacionOferta();
     cargarPantallaContactos();
-}
+    let buttonCarrito = document.getElementById('buttonCarrito');
+    buttonCarrito.onclick = () => {
+        const carritoStr = localStorage.getItem('carrito');
+        if (!carritoStr || carritoStr.length === 0) return;
+        const carritoArr = JSON.parse(carritoStr);
+        if (carritoArr.length === 0) return;
 
+        // bootbox.alert(`Hay ${carritoArr.length} productos en el carrito`);
 
-class Producto {
-    constructor(tipoDeProducto, nombre, precio, stock, imagen) {
-
-        this.tipoDeProducto = tipoDeProducto;
-        this.nombre = nombre;
-        this.precio = precio;
-        this.stock = stock;
-        this.imagen = imagen;
+        fetch('/templates/carrito.ejs').then((res) => {
+            if (res.ok) {
+                return res.text();
+            }
+        }).then((carritoHTML) => {
+            const html = ejs.render(carritoHTML, { carrito: carritoArr });
+            var dialog = bootbox.dialog({
+                // title: 'Mis compras',
+                message: html,
+                size: 'xl',
+                buttons: {
+                    ok: {
+                        label: "Comprar",
+                        className: 'buttonAgregarCarrito',
+                        callback: function () {
+                            // console.log('Custom OK clicked');
+                            bootbox.alert('Se ha comprado los productos');
+                        }
+                    },
+                    limpiarCarrito: {
+                        label: 'Limpiar carrito',
+                        className: 'buttonLimpiarCarrito',
+                        callback: function () {
+                            bootbox.confirm('¿De verdad desea borrar todos los items del carrito?', () => {
+                                limpiarCarrito();
+                            });
+                        }
+                    }
+                }
+            });
+        });
     }
 }
-
-
-
-
-
